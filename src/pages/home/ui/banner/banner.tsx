2b2822a7f7cfd3d82e81c7168ae08872e2dcfc62
Anna <anysofronova@yandex.ru>
2022-08-03 13:19:49 +0300

    style: banner and container 🐻

--- src/pages/home/ui/banner/banner.tsx
+++ src/pages/home/ui/banner/banner.tsx
@@ -1,6 +1,6 @@
 export const Banner = () => {
   return (
-    <div className="absolute inset-x-0 flex flex-col w-full bg-indigo-600 text-white justify-center items-center p-8 gap-2 shadow-inner">
+    <div className="relative inset-x-0 flex flex-col w-full bg-indigo-600 text-white justify-center items-center p-8 gap-2 shadow-inner">
       <h1 className="text-6xl font-semibold">conduit</h1>
       <h3 className="font-extralight text-2xl">
         A place to share your knowledge.
