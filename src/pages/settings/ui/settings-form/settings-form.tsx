a7085d334e283490baa6eb29035530146d1983d2
Anna <anysofronova@yandex.ru>
2022-08-03 11:44:07 +0300

    fix: disabled button ⛑

--- src/pages/settings/ui/settings-form/settings-form.tsx
+++ src/pages/settings/ui/settings-form/settings-form.tsx
@@ -115,7 +115,7 @@ export const SettingsForm = () => {
         />
 
         <button
-          disabled={!isDirty && !isValid}
+          disabled={!(isDirty && isValid)}
           className="ml-auto bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded disabled:opacity-25"
           type="submit"
         >
