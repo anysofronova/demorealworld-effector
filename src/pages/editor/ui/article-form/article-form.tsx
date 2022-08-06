a9328414a6273f22692a0777b0ff7ac241e00b58
Anna <anysofronova@yandex.ru>
2022-08-06 17:17:07 +0300

    feat: article form skeleton 🐝

--- src/pages/editor/ui/article-form/article-form.tsx
+++ src/pages/editor/ui/article-form/article-form.tsx
@@ -17,6 +17,7 @@ import { FormInput, FormTextarea } from '@/shared/ui/molecules'
 import { makeErrors } from '@/shared/utils/makeErrors'
 import { getArticleBySlugFx } from '@/pages/article/model'
 import { $singleArticle, isPending } from '@/pages/article/model/store'
+import { EditorSkeleton } from '@/shared/ui/atoms/editor-skeleton'
 
 type ArticleFormProps = {
   slug: string
@@ -48,7 +49,6 @@ export const ArticleForm = ({ slug }: ArticleFormProps) => {
     }
   }, [article, setValue])
   const isLoading = useStore(isPending)
-  console.log(article)
   const onSubmit: SubmitHandler<ArticleFormFields> = useCallback(
     async (data) => {
       try {
@@ -83,73 +83,77 @@ export const ArticleForm = ({ slug }: ArticleFormProps) => {
         'flex flex-col justify-center w-full justify-center pt-2 pb-1 px-4 max-w-[540px] mx-auto'
       }
     >
-      <form
-        className={
-          'flex flex-col justify-center items-center py-8 gap-4 w-full'
-        }
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <FormInput<ArticleFormFields>
-          id="title"
-          type="text"
-          name="title"
-          label="Article Title"
-          placeholder="Article Title"
-          className="mb-2"
-          rules={{ required: 'This field is required.' }}
-          register={register}
-          errors={errors}
-        />
-
-        <FormInput<ArticleFormFields>
-          id="description"
-          type="text"
-          name="description"
-          label="About"
-          placeholder="What's this article about?"
-          className="mb-2"
-          rules={{ required: 'This field is required.' }}
-          register={register}
-          errors={errors}
-        />
-
-        <FormTextarea<ArticleFormFields>
-          id="body"
-          name="body"
-          label="Body"
-          placeholder="Write your article (in markdown)"
-          className="mb-2"
-          rules={{ required: 'This field is required.' }}
-          register={register}
-          errors={errors}
-        />
-
-        <FormInput<ArticleFormFields>
-          id="tagList"
-          type="text"
-          name="tagList"
-          label="Tags"
-          placeholder="Enter Tags"
-          className="mb-2"
-          register={register}
-          errors={errors}
-        />
-
-        <button
-          disabled={!(isDirty && isValid)}
-          className="ml-auto bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded disabled:opacity-25"
-          type="submit"
+      {isLoading ? (
+        <EditorSkeleton />
+      ) : (
+        <form
+          className={
+            'flex flex-col justify-center items-center py-8 gap-4 w-full'
+          }
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {isSubmitting ? (
-            <div>
-              <CircleIcon />
-              Loading...
-            </div>
-          ) : (
-            'Publish Article'
-          )}
-        </button>
-      </form>
+          <FormInput<ArticleFormFields>
+            id="title"
+            type="text"
+            name="title"
+            label="Article Title"
+            placeholder="Article Title"
+            className="mb-2"
+            rules={{ required: 'This field is required.' }}
+            register={register}
+            errors={errors}
+          />
+
+          <FormInput<ArticleFormFields>
+            id="description"
+            type="text"
+            name="description"
+            label="About"
+            placeholder="What's this article about?"
+            className="mb-2"
+            rules={{ required: 'This field is required.' }}
+            register={register}
+            errors={errors}
+          />
+
+          <FormTextarea<ArticleFormFields>
+            id="body"
+            name="body"
+            label="Body"
+            placeholder="Write your article (in markdown)"
+            className="mb-2"
+            rules={{ required: 'This field is required.' }}
+            register={register}
+            errors={errors}
+          />
+
+          <FormInput<ArticleFormFields>
+            id="tagList"
+            type="text"
+            name="tagList"
+            label="Tags"
+            placeholder="Enter Tags"
+            className="mb-2"
+            register={register}
+            errors={errors}
+          />
+
+          <button
+            disabled={!(isDirty && isValid)}
+            className="ml-auto bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded disabled:opacity-25"
+            type="submit"
+          >
+            {isSubmitting ? (
+              <div>
+                <CircleIcon />
+                Loading...
+              </div>
+            ) : (
+              'Publish Article'
+            )}
+          </button>
+        </form>
+      )}
       <hr className="my-1" />
     </div>
   )
